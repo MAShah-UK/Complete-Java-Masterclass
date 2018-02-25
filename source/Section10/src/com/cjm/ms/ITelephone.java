8fd1481ff9e58befea06ca92d5c061de6690ac7c
MAShah-UK <m.a.shah@live.co.uk>
2018-02-25 22:31:31 +0000

    Started interface exercise

--- source/Section10/src/com/cjm/ms/ITelephone.java
+++ source/Section10/src/com/cjm/ms/ITelephone.java
@@ -1,6 +1,9 @@
 package com.cjm.ms;
 
 public interface ITelephone {
+    // Fields are public by default, and must be public.
+    // Interface methods are public by default, and can't be defined.
+    // Interface methods can be private, but must be defined.
     void powerOn();
     void dial(int phoneNumber);
     void answer();
--- source/Section10/src/com/cjm/ms/Main.java
+++ source/Section10/src/com/cjm/ms/Main.java
@@ -2,14 +2,21 @@ package com.cjm.ms;
 
 public class Main {
     public static void main(String[] args) {
-        createDeskPhone();
+        createPhones();
     }
 
-    public static void createDeskPhone() {
-        ITelephone timsPhone;
-        timsPhone = new DeskPhone(123456); // Polymorphism.
+    // Practice working with interfaces.
+    public static void createPhones() {
+        ITelephone timsPhone; // Can't instantiate an interface, e.g. new ITelephone().
+
+        timsPhone = new DeskPhone(123456); // Uses polymorphism - recommended.
         timsPhone.powerOn();
         timsPhone.callPhone(123456);
+        timsPhone.answer();
+
+        timsPhone = new MobilePhone(24565); // Uses polymorphism again.
+        timsPhone.powerOn();
+        timsPhone.callPhone(24565);
         timsPhone.answer();
     }
 }
