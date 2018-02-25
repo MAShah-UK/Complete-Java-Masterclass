88a1588534403460dcde8676fcdf2f7c6c2b68bf
MAShah-UK <m.a.shah@live.co.uk>
2018-02-25 21:09:11 +0000

    Created ITelephone interface and DeskPhone class

--- source/Section10/src/com/cjm/ms/Main.java
+++ source/Section10/src/com/cjm/ms/Main.java
@@ -2,6 +2,14 @@ package com.cjm.ms;
 
 public class Main {
     public static void main(String[] args) {
+        createDeskPhone();
+    }
 
+    public static void createDeskPhone() {
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(123456); // Polymorphism.
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
     }
 }
