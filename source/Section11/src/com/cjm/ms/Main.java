62bbd5c34294b07802b6af3c01669e65e717248a
MAShah-UK <m.a.shah@live.co.uk>
2018-03-08 18:56:23 +0000

    Covered unbounded and bounded type parameters

--- source/Section11/src/com/cjm/ms/Main.java
+++ source/Section11/src/com/cjm/ms/Main.java
@@ -1,27 +1,57 @@
 package com.cjm.ms;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        printNumbers();
+        printObjList();
+        createTeam();
     }
 
-    // Practice work with generics.
-    public static void printNumbers() {
+    // Practice working with raw types.
+    public static void printObjList() {
+        System.out.println("BEGIN: printObjList");
+
         // If type parameter isn't specified it defaults to Object.
         // The list can now accept any datatype due to upcasting,
-        // but this is not recommended since it breaks type checking.
-        Arraylist items = new ArrayList();
-        items.add(1);
-        items.add(2);
-        items.add(3);
-        items.add(4);
-        items.add(5);
+        // but this is not recommended since it can break type checking.
+        List objList = new ArrayList(); // Equivalent: List<Object> items = new ArrayList<Object>();
+        objList.add(1); // Integer upcasted to Object.
+        objList.add(2);
+        objList.add(3);
+        objList.add("four"); // String upcasted to Object.
+        objList.add(5);
 
-        for (Object i : n) {
-            System.out.println((Integer) i * 2); // 2, 4, 6, 8, 10.
+        for (Object i : objList) {
+            // Will give a ClassCastException for 4th element.
+            // System.out.println((Integer) i);
+            System.out.println(i.toString()); // Integer and String override Object.toString().
         }
+    }
+
+    // Practice creating generic classes.
+    public static void createTeam() {
+        System.out.println("\nBEGIN: createTeam");
+
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        // adelaideCrows.addPlayer(pat); // Error since FootballPlayer was specified.
+        // adelaideCrows.addPlayer(beckham); // Same as above.
+
+        Team<BaseballPlayer> chicagoCubs = new Team<>("Chicago Cubs");
+        chicagoCubs.addPlayer(pat);
+
+        Team<SoccerPlayer> UKTeam = new Team<>("UKTeam");
+        UKTeam.addPlayer(beckham);
+
+        // This code will execute without bounded type parameters.
+        // Team<String> brokenTeam = new Team<>("This won't work.");
+        // brokenTeam.addPlayer("no-one");
     }
 }
---
+++ source/Section11/src/com/cjm/ms/Player.java
@@ -0,0 +1,99 @@
+package com.cjm.ms;
+
+import java.util.ArrayList;
+
+public abstract class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+// Should be in it's own file, but I don't want to disorganise code and packages haven't been covered yet.
+class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
+
+class FootballPlayer extends Player {
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
+
+class SoccerPlayer extends Player {
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+}
+
+// The <T> means that the class requires a type parameter argument  that will be stored as T.
+// class Team<T> {
+// Bounded type parameter that only allows subclasses of Player to be specified.
+class Team<T extends Player> {
+    private String name;
+    private int played;
+    private int won;
+    private int lost;
+    private int tied;
+
+    // This is bad code since it allows members of any sport to join the same team.
+    // This is nonsensical as a team usually consists of members of the same sport.
+    // private ArrayList<Player> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addPlayer(T player) {
+        if (members.contains(player)) {
+            // For class Team<T>:
+            // Error since T can be any type so the compiler doesn't know if it contains getName().
+            // System.out.println(player.getName() + " is already on the team.");
+            // Works since the compiler knows that Player contains it, but is bad code quality.
+            // System.out.println( ((Player) player).getName() + " is already on the team.");
+
+            // For class Team<T extends Player>:
+            System.out.println(player.getName() + " is already on the team.");
+        } else {
+            members.add(player);
+            System.out.println(player.getName() + " picked for team " + name);
+        }
+    }
+
+    public int numPlayers() {
+        return members.size();
+    }
+
+    public void matchResult(Team opponent, int ourScore, int theirScore) {
+        if (ourScore > theirScore) {
+            won++;
+        } else if (ourScore < theirScore) {
+            lost++;
+        } else {
+            tied++;
+        }
+        played++;
+        if (opponent != null) { // Prevents infinite loop.
+            opponent.matchResult(null, theirScore, ourScore);
+        }
+    }
+
+    public int ranking() {
+        return (won * 2) + tied;
+    }
+}
+
+
+
