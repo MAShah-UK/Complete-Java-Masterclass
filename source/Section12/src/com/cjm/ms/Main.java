4648968afcaa4345ea523a8215b6365e1f39c598
MAShah-UK <m.a.shah@live.co.uk>
2018-03-14 00:04:28 +0000

    Started covering packages

--- source/Section12/src/com/cjm/ms/Main.java
+++ source/Section12/src/com/cjm/ms/Main.java
@@ -1,7 +1,16 @@
 package com.cjm.ms;
 
+import javafx.scene.Node;
+
 public class Main {
     public static void main(String[] args) {
-
+        // Defined in:
+        // javax.xml.soap.Node
+        // com.sun.tools.jdeps.DepsAnalyzer.Node
+        // javafx.scene.Node
+        Node node1 = null;
+        // Can also specify package prior to class:
+        javafx.scene.Node node2 = null;
+        java.util.List<String> list = new java.util.ArrayList<>();
     }
 }
