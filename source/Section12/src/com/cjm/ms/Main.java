6025244c9704475a54f1d427f46f26f9468a8e00
MAShah-UK <m.a.shah@live.co.uk>
2018-03-24 13:46:10 +0000

    Saved console output

--- source/Section12/src/com/cjm/ms/Main.java
+++ source/Section12/src/com/cjm/ms/Main.java
@@ -15,12 +15,12 @@ import static com.cjm.Math.Series.*;
 public class Main {
     public static void main(String[] args) {
         importPackages();
-        //createWindow();
+        createWindow();
         createHelloWorld();
-        //manageData();
+        manageData();
         useMathLib();
         createScopeCheck();
-        //createX();
+        createX();
         createAccount();
         createStaticTest();
         createFinalTest();
