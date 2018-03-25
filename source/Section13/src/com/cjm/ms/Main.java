be466461594225fc087be896554d88ffda388a6e
MAShah-UK <m.a.shah@live.co.uk>
2018-03-25 12:48:07 +0100

    Completed collections overview

--- source/Section13/src/com/cjm/ms/Main.java
+++ source/Section13/src/com/cjm/ms/Main.java
@@ -3,6 +3,19 @@ package com.cjm.ms;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        createTheatre();
+    }
+
+    // Practice
+    public static void createTheatre() {
+        System.out.println("BEGIN: createTheatre");
+
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+        theatre.getSeats();
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay.");
+        } else {
+            System.out.println("Sorry, seat is taken.");
+        }
     }
 }
