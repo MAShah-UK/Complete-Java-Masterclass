c197bdca3fb500d612aa45487ab688637f011079
MAShah-UK <m.a.shah@live.co.uk>
2018-03-26 16:41:48 +0100

    Completed binary search video

--- source/Section13/src/com/cjm/ms/Theatre.java
+++ source/Section13/src/com/cjm/ms/Theatre.java
@@ -1,15 +1,21 @@
 package com.cjm.ms;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    private List<Seat> seats = new LinkedList<>(); // Changed from List<Seat> seats = new ArrayList<>();
 
-    private class Seat {
+    private class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
+
+        @Override
+        public int compareTo(Seat seat) {
+            return seatNumber.compareTo(seat.getSeatNumber());
+        }
 
         public Seat(String seatNumber) {
             this.seatNumber = seatNumber;
@@ -57,19 +63,26 @@ public class Theatre {
     }
 
     public boolean reserveSeat(String seatNumber) {
-        Seat requestedSeat = null;
-        for (Seat seat : seats) {
-            if (seat.getSeatNumber().equals(seatNumber)) {
-                requestedSeat = seat;
-                break;
-            }
-        }
-        if (requestedSeat == null) {
-            System.out.println("There is no seat " + seatNumber);
+        Seat requestedSeat = new Seat(seatNumber);
+        int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
+        if (foundSeat >= 0) {
+            return seats.get(foundSeat).reserve();
+        } else {
+            System.out.println("There is no seat " + seatNumber + ".");
             return false;
         }
-
-        return requestedSeat.reserve();
+//        for (Seat seat : seats) {
+//            if (seat.getSeatNumber().equals(seatNumber)) {
+//                requestedSeat = seat;
+//                break;
+//            }
+//        }
+//        if (requestedSeat == null) {
+//            System.out.println("There is no seat " + seatNumber);
+//            return false;
+//        }
+//
+//        return requestedSeat.reserve();
     }
 
     // For testing.
