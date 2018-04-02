94be58123f56777d0f7674a9c1e48650f86bfd29
MAShah-UK <m.a.shah@live.co.uk>
2018-04-02 17:56:00 +0100

    Covered immutable classes
    Set fields as private/final.
    Set class as final.
    Avoid setters.
    Do not directly set field to argument in constructor. Create a copy.

--- source/Section13/src/com/cjm/ms/Adventure.java
+++ source/Section13/src/com/cjm/ms/Adventure.java
@@ -9,10 +9,12 @@ class Location {
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>();
+        // To make a class immutable avoid setters and copy arguments rather than directly setting them.
+        // The calling class may still have a reference to exits which would allow changing it after the fact.
+        this.exits = new HashMap<>(exits);
         this.exits.put("Q", 0);
     }
 
@@ -28,38 +30,39 @@ class Location {
         // Prevents outside classes from altering the map within this object.
         return new HashMap<>(exits);
     }
-
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
 }
 
 public class Adventure {
-    private final Map<String, String> vocabulary;
     private Map<Integer, Location> locations = new HashMap<>();
 
-    public Adventure() {        
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java."));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building."));
-        locations.put(2, new Location(2, "You are at the top of a hill."));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring."));
-        locations.put(4, new Location(4, "You are in a valley beside a stream."));
-        locations.put(5, new Location(5, "You are in the forest."));
+    public Adventure() {
+        Map<String, Integer> tmpExit = new HashMap<>();
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java.", tmpExit));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
+        tmpExit = new HashMap<>();
+        tmpExit.put("W", 2);
+        tmpExit.put("E", 3);
+        tmpExit.put("S", 4);
+        tmpExit.put("N", 5);
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building.", tmpExit));
 
-        locations.get(2).addExit("N", 5);
+        tmpExit = new HashMap<>();
+        tmpExit.put("N", 5);
+        locations.put(2, new Location(2, "You are at the top of a hill.", tmpExit));
 
-        locations.get(3).addExit("W", 1);
+        tmpExit = new HashMap<>();
+        tmpExit.put("W", 1);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring.", tmpExit));
 
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
+        tmpExit = new HashMap<>();
+        tmpExit.put("N", 1);
+        tmpExit.put("W", 2);
+        locations.put(4, new Location(4, "You are in a valley beside a stream.", tmpExit));
 
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
+        tmpExit = new HashMap<>();
+        tmpExit.put("S", 1);
+        tmpExit.put("W", 2);
+        locations.put(5, new Location(5, "You are in the forest.", tmpExit));
 
         start();
     }
@@ -98,26 +101,30 @@ public class Adventure {
     }
 
     public String parseInput(String input) {
-        input = input.trim().toUpperCase();
-        String[] words = input.split(" ");
+        String output = "";
+        String[] words = input.trim().toUpperCase().split(" ");
         for (String word: words) {
             switch (word) {
                 case "NORTH": case "N":
-                    return "N";
+                    output = "N";
                     break;
                 case "EAST":  case "E":
-                    return "E";
+                    output = "E";
                     break;
                 case "SOUTH": case "S":
-                    return "S";
+                    output = "S";
                     break;
                 case "WEST":  case "W":
-                    return "W";
+                    output = "W";
                     break;
                 case "QUIT":  case "Q":
-                    return "Q";
+                    output = "Q";
                     break;
             }
+            if (!output.isEmpty()) {
+                break;
+            }
         }
+        return output;
     }
 }
--- source/Section13/src/com/cjm/ms/MapProgram.java
+++
@@ -1,5 +0,0 @@
-package com.cjm.ms;
-
-public class MapProgram {
-
-}
