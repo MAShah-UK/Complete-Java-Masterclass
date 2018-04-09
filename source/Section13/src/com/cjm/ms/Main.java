c9c65f7e997669417a79b5a882fa54088a48c1b1
MAShah-UK <m.a.shah@live.co.uk>
2018-04-09 21:46:16 +0100

    Saved console output and completed section 13

--- source/Section13/src/com/cjm/ms/Main.java
+++ source/Section13/src/com/cjm/ms/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         createTheatre();
         createMap();
-        //createAdventure();
+        createAdventure();
         createHeavenlyBodies();
         findSquaresCubes();
         createBasket();
