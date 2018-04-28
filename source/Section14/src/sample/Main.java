7dedd1c0d6fd45855a577709297b99114e9853f6
MAShah-UK <m.a.shah@live.co.uk>
2018-04-28 21:43:46 +0100

    Created JavaFX UI elements through code and fxml file.

--- source/Section14/src/sample/Main.java
+++ source/Section14/src/sample/Main.java
@@ -2,16 +2,45 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+
+//        // Equivalent to .fxml:
+//        <GridPane fx:controller="sample.Controller"
+//                xmlns:fx="http://javafx.com/fxml" alignment="center" hgap="10" vgap="10">
+//            <Label text="Welcome to JavaFX!" textFill="blue">
+//                <font>
+//                    <Font name="Times New Roman bold" size="70"/>
+//                </font>
+//            </Label>
+//        </GridPane>
+
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//
+//        Label greeting = new Label("Welcome to JavaFX!");
+//        greeting.setTextFill(Color.BLUE);
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+//        root.getChildren().add(greeting);
+
+        primaryStage.setTitle("Hello JavaFX");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
