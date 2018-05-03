61ccdc6fbb00f66e893c62a1187ae5ee971fe7c0
M. Shah <m.a.shah@live.co.uk>
2018-05-03 15:41:51 +0100

    Covered basic multithreading with JavaFX.

--- source/Section14/src/sample/Controller.java
+++ source/Section14/src/sample/Controller.java
@@ -1,9 +1,11 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -15,6 +17,8 @@ public class Controller {
     private Button byeButton;
     @FXML
     private CheckBox clearCheckBox;
+    @FXML
+    private Label ourLabel;
 
     @FXML // Optional before a method, but indicates that it is related to the FXML file.
     public void initialize() { // initialize() is automatically called.
@@ -22,7 +26,7 @@ public class Controller {
         byeButton.setDisable(true);
     }
     @FXML
-    public void onButtonClicked(ActionEvent e) {
+    public void onButtonClicked(ActionEvent e) { // Automatically called when button is clicked.
         String prefix = "[?]";
         if(e.getSource().equals(helloButton)) {
             prefix = "Hello";
@@ -31,6 +35,31 @@ public class Controller {
         }
         System.out.println(prefix + ", " + nameField.getText() + ".");
 
+        // Runs complicated process on another thread so that the UI doesn't suspend.
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // Used to debug which thread the code is running on.
+                    String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                    System.out.println("I'm going to sleep: " + s);
+                    // Complicated process represented by Thread.sleep().
+                    Thread.sleep(10000);
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                            System.out.println("I'm updating the label: " + s);
+                            // UI controls can only be updated on UI thread which runLater() does.
+                            ourLabel.setText("We did something.");
+                        }
+                    });
+                } catch(InterruptedException ie) {
+
+                }
+            }
+        }).start();
+
         if(clearCheckBox.isSelected()) {
             nameField.clear();
             helloButton.setDisable(true);
@@ -38,7 +67,7 @@ public class Controller {
         }
     }
     @FXML
-    public void handleKeyReleased() {
+    public void handleKeyReleased() { // Automatically called when text is input into text field.
         String text = nameField.getText();
         boolean disableButtons = text.trim().isEmpty();
         helloButton.setDisable(disableButtons);
@@ -46,7 +75,7 @@ public class Controller {
     }
 
     @FXML
-    public void handleChange() {
+    public void handleChange() { // Automatically called when checkbox is interacted with.
         String postfix = clearCheckBox.isSelected() ? "checked" : "not checked";
         System.out.println("CheckBox is " + postfix + ".");
     }
