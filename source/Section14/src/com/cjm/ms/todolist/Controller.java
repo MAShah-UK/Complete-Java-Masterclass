d1a4c50c2b30a126a947004f2747ab6b83a8c391
M. Shah <m.a.shah@live.co.uk>
2018-05-07 23:56:29 +0100

    Covered DialogPane.

--- source/Section14/src/com/cjm/ms/todolist/Controller.java
+++ source/Section14/src/com/cjm/ms/todolist/Controller.java
@@ -5,11 +5,12 @@ import com.cjm.ms.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
@@ -25,6 +26,8 @@ public class Controller {
     private TextArea itemDetailsTextArea;
     @FXML
     private Label deadlineLabel;
+    @FXML
+    private BorderPane mainBorderPane;
 
     @FXML
     public void initialize() {
@@ -63,6 +66,18 @@ public class Controller {
         todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
+    }
+    @FXML
+    public void showNewItemDialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("todoItemDialog.fxml"));
+            dialog.getDialogPane().setContent(root);
+        } catch(IOException e) {
+            System.out.println("Couldn't load the dialog: ");
+            e.printStackTrace();
+        }
     }
     @FXML
     public void handleClickListView() {
---
+++ source/Section14/src/com/cjm/ms/todolist/DialogController.java
@@ -0,0 +1,4 @@
+package com.cjm.ms.todolist;
+
+public class DialogController {
+}
