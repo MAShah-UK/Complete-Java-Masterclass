55ba2aeb2bbdc1c089e4d4546504943f851ad6f0
M. Shah <m.a.shah@live.co.uk>
2018-05-09 13:42:05 +0100

    Added key binding to delete items, and a toolbar with a new item button.

--- source/Section14/src/com/cjm/ms/todolist/Controller.java
+++ source/Section14/src/com/cjm/ms/todolist/Controller.java
@@ -8,8 +8,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
@@ -141,6 +142,13 @@ public class Controller {
             // ListView is automatically updated since it has an ObservableArrayList.
 //            todoListView.setItems(TodoData.getInstance().getTodoItems());
             todoListView.getSelectionModel().select(newItem);
+        }
+    }
+    @FXML
+    public void handleKeyPressed(KeyEvent keyEvent) {
+        TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+        if (selectedItem != null && keyEvent.getCode().equals(KeyCode.DELETE)) {
+            deleteItem(selectedItem);
         }
     }
     @FXML
