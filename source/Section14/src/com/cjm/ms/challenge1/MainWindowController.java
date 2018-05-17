4ca3a39661b5e67d33eb5ddff885141242e11050
M. Shah <m.a.shah@live.co.uk>
2018-05-17 20:33:58 +0100

    Designed ContactInfoDialog.

--- source/Section14/src/com/cjm/ms/challenge1/MainWindowController.java
+++ source/Section14/src/com/cjm/ms/challenge1/MainWindowController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class Controller {
+public class MainWindowController {
     @FXML
     private TableView contactsTableView;
 
