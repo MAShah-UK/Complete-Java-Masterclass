124be06bb4a737b41d0ff6e400660329893b6ba3
M. Shah <m.a.shah@live.co.uk>
2018-05-29 15:26:21 +0100

    Binded a control's items property to a task's value property to dynamically update its contents.

--- source/Section16/src/com/cjm/ms/javafxconcurrency/Controller.java
+++ source/Section16/src/com/cjm/ms/javafxconcurrency/Controller.java
@@ -4,25 +4,38 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class Controller {
     private Task<ObservableList<String>> task;
 
+    @FXML
+    private ListView listView;
+
     public void initialize() {
-        task = new Task<ObservableList<String>>() {
+        task = new Task<>() {
             @Override
             protected ObservableList<String> call() throws Exception {
-                // Simulates getting data from database.
+                // Simulates retrieving data from database.
                 Thread.sleep(1000);
-                return FXCollections.observableArrayList(
+                ObservableList<String> employees = FXCollections.observableArrayList(
                         "Tim Buchalka",
                         "Bill Rogers",
                         "Jack Jill",
                         "Joan Andrews",
                         "Mary Johnson",
                         "Bob McDonald");
+//                // This code can be avoided by binding the ListView's itemsProperty to the task's valueProperty.
+//                Platform.runLater(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        listView.setItems(employees);
+//                    }
+//                });
+                return employees;
             }
         };
+        listView.itemsProperty().bind(task.valueProperty());
     }
     @FXML
     public void buttonPressed() {
