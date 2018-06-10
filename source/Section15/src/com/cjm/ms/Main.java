136198247982cddf74fa77cc9fa7ee2f6210ea9a
MAShah-UK <m.a.shah@live.co.uk>
2018-06-10 22:10:36 +0100

    Covered basics of exception handling.

--- source/Section15/src/com/cjm/ms/Main.java
+++ source/Section15/src/com/cjm/ms/Main.java
@@ -1,8 +1,72 @@
 package com.cjm.ms;
 
-public class Main {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        divideLBYL(50, 0);
+        divideEAFP(50, 0);
+        getInt();
+        getIntLBYL();
+    }
+    /*
+        Uses 'look before you leap' concept in which you ensure
+        that the arguments are valid before using them.
+     */
+    private static int divideLBYL(int x, int y) {
+        // Division by zero is undefined.
+        if(y != 0) {
+            return x/y;
+        } else {
+            System.out.println("Can't divide by zero.");
+            // Returning 0 is technically incorrect.
+            return 0;
+        }
+    }
+    /*
+        Uses 'easy to ask for forgiveness than permission' concept
+        in which you execute code then deal with potential fallout.
+     */
+    private static int divideEAFP(int x, int y) {
+        try {
+            return x/y;
+        } catch(ArithmeticException e) {
+            System.out.println("Can't divide by zero.");
+            return 0;
+        }
+    }
+    // Gets input from user. Expects integer, otherwise will crash.
+    private static int getInt() {
+        System.out.print("Enter an integer (no validation, might crash): ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+    // Validates input. Returns 0 for invalid input.
+    private static int getIntLBYL() {
+        System.out.print("Enter an integer (validated, won't crash): ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        for(char symbol : input) {
+            if (!Character.isDigit(symbol)) {
+                System.out.println("Invalid input.");
+                return 0;
+            }
+        }
+        System.out.println("Valid input.");
+        return Integer.parseInt(input);
+    }
+    // Also validates input, but uses exceptions.
+    private static int getIntEAFP() {
+        System.out.print("Enter an integer (validated, won't crash): ");
+        Scanner scanner = new Scanner(System.in);
+        int value = 0;
+        try {
+            int value = scanner.nextInt();
+            System.out.println("Valid input.");
+        } catch(InputMismatchException e) {
+            System.out.println("Invalid input.");
+        }
+        return value;
     }
 }
