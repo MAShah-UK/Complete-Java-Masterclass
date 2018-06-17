d53089ae82120629b92c37353c482dfd7a7f78d0
MAShah-UK <m.a.shah@live.co.uk>
2018-06-17 08:10:29 +0100

    Started covering IO using adventure game from previous exercise.

--- source/Section15/src/com/cjm/ms/Main.java
+++ source/Section15/src/com/cjm/ms/Main.java
@@ -1,10 +1,14 @@
 package com.cjm.ms;
 
+import java.util.HashMap;
 import java.util.InputMismatchException;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("BEGIN: Exceptions");
+
         System.out.println("The result of 50/0 is: ");
         System.out.println(divideLBYL(50, 0));
         System.out.println();
@@ -28,6 +32,10 @@ public class Main {
             System.out.println(e.getMessage());
         }
         System.out.println();
+
+        System.out.println("\nBEGIN: IO");
+
+        createAdventureGame();
     }
     /*
         Uses 'look before you leap' concept in which you ensure
@@ -108,5 +116,51 @@ public class Main {
         }
         System.out.println("Valid input.");
         return value;
+    }
+    private static void createAdventureGame() {
+        Locations locations = new Locations();
+
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> vocabulary = new HashMap<>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+
+        int loc = 1;
+        while(true) {
+            System.out.println(locations.get(loc).getDescription());
+
+            if(loc == 0) {
+                break;
+            }
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for(String exit: exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if(direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for(String word: words) {
+                    if(vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+
+            if(exits.containsKey(direction)) {
+                loc = exits.get(direction);
+
+            } else {
+                System.out.println("You cannot go in that direction");
+            }
+        }
     }
 }
