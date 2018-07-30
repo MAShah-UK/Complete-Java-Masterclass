62d7d0f1794a32a36155ae09594809804e988ad8
MAShah-UK <m.a.shah@live.co.uk>
2018-07-30 13:48:48 +0100

    Covered debugger and its UI, breakpoints, watches, watchpoints, smart step into, etc.

--- source/Section19/src/com/cjm/ms/Main.java
+++ source/Section19/src/com/cjm/ms/Main.java
@@ -4,11 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         StringUtilities utilities = new StringUtilities();
+
+        // Used debugger and its UI, breakpoints, watches, and watchpoints.
         StringBuilder builder = new StringBuilder();
         // Infinite loop leads to OutOfMemoryError.
         while(builder.length() < 10) {
             utilities.addChar(builder, 'a');
         }
         System.out.println(builder);
+
+        // Used smart step into
+        String str = "abcdefg";
+        String result = utilities.upperAndPrefix(utilities.addSuffix(str));
     }
 }
--- source/Section19/src/com/cjm/ms/StringUtilities.java
+++ source/Section19/src/com/cjm/ms/StringUtilities.java
@@ -2,10 +2,24 @@ package com.cjm.ms;
 
 public class StringUtilities {
     private StringBuilder builder = new StringBuilder();
+    // Setting a field watchpoint will suspend execution anytime
+    // the field is modified.
     private int charsAdded = 0;
     public void addChar(StringBuilder builder, char c) {
         // Bug: Source StringBuilder is never modified.
-        this.builder.append(c);
+        // A breakpoint can be set for a specific statement, by clicking
+        // on that line's gutter.
+        // The debugger makes it clear that the instance variable
+        // is being updated rather than the provided argument.
+        // this.builder.append(c); // Bug.
+        builder.append(c);
         charsAdded++;
+    }
+    public String upperAndPrefix(String str) {
+        String upper = str.toUpperCase();
+        return "Prefix_" + upper;
+    }
+    public String addSuffix(String str) {
+        return str + "_Suffix";
     }
 }
