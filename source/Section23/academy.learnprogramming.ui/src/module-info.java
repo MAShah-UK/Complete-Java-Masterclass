b0b0b830c1c020829beee7029e9c4cb18a0d9429
M. Shah <m.a.shah@live.co.uk>
2018-08-09 20:36:50 +0100

    Covered requires transitive statement and finished section 23.

--- source/Section23/academy.learnprogramming.ui/src/module-info.java
+++ source/Section23/academy.learnprogramming.ui/src/module-info.java
@@ -1,8 +1,10 @@
 module academy.learnprogramming.ui {
-    requires javafx.graphics;
+    // Optional since javafx.controls marks it as transitive.
+    // requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
-    requires academy.learnprogramming.common;
+    // Optional since academy.learnprogramming.db marks it as transitive.
+    // requires academy.learnprogramming.common;
     requires academy.learnprogramming.db;
 
     opens academy.learnprogramming.ui to javafx.graphics, javafx.fxml;
